b0f939ee4aeb555936726733fdcdd577ba9d8e54
mattcomi <mattcomi@gmail.com>
2017-12-08 18:33:41 +0800

    Removed travis integration

--- travis.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-xctool -project PersistentObject.xcodeproj -scheme "PersistentObject Mac" test
-xctool -project PersistentObject.xcodeproj -scheme "PersistentObject iOS" -sdk iphonesimulator9.3 -destination "platform=iOS Simulator,name=iPhone 6" test
